fd4ce01e27dfcf0b121fe98e6603d210ed9d1391
Beau Bouchard <beau@beaubouchard.com>
2014-06-22 13:02:33 -0400

    changed layout of config
    changed layout of config to allow database credentials,

--- src/config.js
+++ src/config.js
@@ -12,4 +12,24 @@ config.channels = ['#general'];
 
 config.commandChar = '!';
 
+config.production: {
+    //url to be used in link generation
+    url: 'https://www.wh.iterabb.it/',
+    //exturnal database connection
+    MySQL: {
+        use: false,  //set to true to use a MySQL database.
+        host: '127.0.0.1',
+        port: '27017',
+        db:   'dbnamehere',
+        User: 'readUsername',
+        Pass: ''//Kate will prompt for passwords, then save it to here. 
+    },
+    dbuser
+    //server details
+    server: {
+        host:   '127.0.0.1',
+        port:   '3421'
+    }
+}
+
 module.exports = config;
