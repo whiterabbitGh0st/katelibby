3cb4e5b51fc800355e8e025f5d77efeccfaec981
Beau Bouchard <beau@beaubouchard.com>
2014-06-23 21:54:45 -0400

    removed mysql stuff using separate file

--- src/config.js
+++ src/config.js
@@ -12,24 +12,4 @@ config.channels = ['#general'];
 
 config.commandChar = '!';
 
-config.production: {
-    //url to be used in link generation
-    url: 'https://www.wh.iterabb.it/',
-    //exturnal database connection
-    MySQL: {
-        use: false,  //set to true to use a MySQL database.
-        host: '127.0.0.1',
-        port: '27017',
-        db:   'dbnamehere',
-        user: 'readUsername',
-        pass: ''//Kate will prompt for passwords, then save it to here. 
-    },
-    dbuser
-    //server details
-    server: {
-        host:   '127.0.0.1',
-        port:   '3421'
-    }
-}
-
 module.exports = config;
