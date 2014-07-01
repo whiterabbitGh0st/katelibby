49b0a1fa4e6a02744b8d271864ea6b7dc279a883
Beau Bouchard <beau@beaubouchard.com>
2014-07-01 00:25:34 -0400

    Update remind.js

--- src/commands/remind.js
+++ src/commands/remind.js
@@ -1,6 +1,7 @@
 module.exports = function(katelibby, target, from, args) {
   var diff = args.split(" ");
   var when = new Date(new Date().getTime() + parseInt(diff[0])*60000);
-  katelibby.addEvent(when, from+":"+args.substr(args.charAt(' '),args.length) , target,function(to,msg){ katelibby.say(to,msg); });
+  diff.splice(0,1);
+  katelibby.addEvent(when, from+": "+ diff.join(" "), target,function(to,msg){ katelibby.say(to,msg); });
   return "Alright, I will remind you at " + when.toString();
 };
