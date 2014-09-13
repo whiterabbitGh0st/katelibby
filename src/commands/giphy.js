26605f9d4d146cfc21e6753bc3aaa6bf5471c3c2
Beau Bouchard <bnb2011@gmail.com>
2014-09-13 01:28:29 -0400

    Update giphy.js

--- src/commands/giphy.js
+++ src/commands/giphy.js
@@ -8,14 +8,14 @@ module.exports = function(katelibby, target, from, args) {
     var url = api_url + querystring.stringify({
         q: args,
         api_key: api_key,
-        limit: 1,
+        limit: 10,
         offset: 0
     });
 
     request({url: url, json: true}, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             if (typeof body.data[0] === 'undefined') { katelibby.say(target, "Are you trying to make me crash?");}
-            else { katelibby.say(target, body.data[0].images.original.url);}
+            else { katelibby.say(target, body.data[Math.floor(Math.random() * (10 + 1))].images.original.url);}
         }
     });
 };
