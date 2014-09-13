a06a7aa0bd7562d1e6ac44c9fb008e1f353bb78e
Beau Bouchard <bnb2011@gmail.com>
2014-09-13 00:17:07 -0400

    Update tweet.js

--- src/commands/tweet.js
+++ src/commands/tweet.js
@@ -4,7 +4,7 @@ var config = require("../twitter.js");
 module.exports = function katetweet(katelibby, target, from, args, callback) {
     var twit = new Twit(config);
     if(typeof args !== 'string') {
-        return 'What the fuck is this shit?';
+        return 'wut';
     } else if(args.length > 125) {
         return 'Tweet is too long: ' + args.length;
     } else {
