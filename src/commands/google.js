35e97defd2ffdfbde1e29bfdc3a76d88c08fd4a0
Beau Bouchard <bnb2011@gmail.com>
2015-04-16 17:50:31 -0400

    Update google.js

--- src/commands/google.js
+++ src/commands/google.js
@@ -8,13 +8,17 @@ module.exports = function kateGoogle(katelibby, target, from, args) {
             console.error(err);
             return;
         }
-
-        if (links && links.length) {
+        
+        if (!links[0].title.substring(0,3).localeCompare('News')) {
+            katelibby.say(target, links[1].title + ' - ' + links[1].href);
+            katelibby.say(target, links[1].description);
+        } else if (links && links.length) {
             katelibby.say(target, links[0].title + ' - ' + links[0].href);
             katelibby.say(target, links[0].description);
         } else {
-            katelibby.say(target, 'Nothing found, try harder.');
+            
         }
+
     });
 
     return 'searching ...';
